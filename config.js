0da1894846a8b406ab505c45939132a45588c674
FatLe2k5 <phatle17022005@gmail.com>
2022-03-06 20:48:27 +0700

    message

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hè Nhô ! 🖐🖐🖐',
+    introDesc: `Anh có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'OKii',
+    title: 'Anh biết Em thích Anh phải không phải không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích Anh đó nha :> ',
+    btnYes: 'Có, em thật sự thích anh rất nhiều 💖💖💖',
+    btnNo: '💔 Anh bị mơ à ! 💔 ',
+    question:'Nói cho Anh biết đi vì sao Em thích anh 😊😊',
+    reasonPlaceholder: 'Lý do ii',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh đẹp trai, dễ thương nữa Ahiiiii 😊😊',
+    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000.000.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha bé. 💬💬',
+    btnAccept: 'Okii lun 💖',
+    messLink: 'https://www.facebook.com/messages/t/100030017703572'
+}
